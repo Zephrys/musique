5ed1236757a2f072e5433715602490041405e35e
Prabhjyot Singh Sodhi <prabhjyotsingh95@gmail.com>
2016-02-07 14:09:01 +0530

    sab chal raha hai, shubhu a hagde database

--- api.py
+++ api.py
@@ -6,9 +6,11 @@ from flask.ext.httpauth import HTTPBasicAuth
 import soundcloud
 from key import client_id
 from pprint import pprint
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import json
 from get_from_azure import get_from_azure
+import random
+from decorator import crossdomain
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,14 +21,15 @@ login_manager.init_app(app)
 
 
 @login_manager.user_loader
-def load_user(username):
-    return User.get(username)
+def load_user(id):
+    print id
+    return User.get(id)
 
 
 mongoclient = MongoClient('mongodb://localhost:27017/')
 sinder = mongoclient.sinder
 
-# create a client object with your app credentials
+# create a pclient object with your app credentials
 client = soundcloud.Client(client_id=client_id)
 
 @app.before_request
@@ -34,105 +37,201 @@ def before_request():
     g.user = current_user
 
 
-class User():
-    
+class User(UserMixin):
+    _id = 0
     username = ""
     password = ""
-    is_auth = False
+    # is_auth = False
 
     def __init__(self, username, password):
+        self._id = username
         self.username = username
         self.password = password
-        self.is_auth = False
+        # self.is_auth = False
 
     @classmethod
     def get(cls, id):
-        return sinder.userdb.find_one({'username': id})
+        obj = sinder.userdb.find_one({'_id': id})
+        return User(obj['username'], obj['password'])
 
     def get_id(self):
-        return unicode(sinder.userdb.find_one({'username': self.username})['username'])
-
-    def is_anonymous(self):
-        return False
-
-    def is_active(self):
-        return True
-
-    def is_authenticated(self):
-        return self.is_auth
+        print("returning this from get_id" + unicode(self._id))
+        return unicode(self._id)
 
 
 @app.route('/', methods=['GET'])
 def see():
     return render_template('index.html')
 
+@app.route('/getTrack', methods=['GET'])
+@crossdomain(origin='*')
+def get_stream_url(username=""):
+    """
+    :param track_no: track number as on sound cloud
+    :return: fetch and return image, stream url, artist info from sound cloud
+    """
+    track_no = request.args['track_no']
+
+    track = client.get('/tracks/' + str(track_no))
+
+    # get the tracks streaming URL
+    stream_url = client.get(track.stream_url, allow_redirects=False)
+
+
+    return jsonify({'stream_url': stream_url.location}),200
+
+
+
+
 
 @app.route('/swipe/')
+@login_required
 def swipe():
 
-    return render_template('random.html', song_dict=get_track())
+    return render_template('random.html', song_dict=get_track_details())
 
 
-@app.route('/api/')
-def get():
-    return jsonify({'Yo': 'We are here! :D'}), 200
-
-
-@app.route('/track/', methods=['GET'])
+@app.route('/track/', methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def rate_track():
     """
     :return: save info for user and track and calls get track for next track
     """
-    rating = request.args['rating']
+    rating = request.args['dir']
     username = request.args['username']
+    print request.args
 
-#      put info in recommendation matrix
+    track_no = request.args['track_no']
 
-    get_track()
+    sinder.entries.update({'track_no': track_no, 'username': username}, {'username': username, 'track_no': track_no, 'dir': 1 if rating == "right" else 0}, upsert=True)
+
+    if rating == "right":
+        insert_track = sinder.tracklist.find_one({'track_no': track_no})
+        sinder.playlist.update({'track_no': track_no, 'username': username}, {'username': username,
+                                   'track_no': track_no, 'title': insert_track['title'],
+                                   'artwork_url': insert_track['artwork_url'],
+                                   'artist': insert_track['artist']}, upsert=True)
+
+    return get_track(username)
 
 
-@app.route('/track/', methods=['GET'])
+@app.route('/playlist/', methods=['GET'])
 @login_required
-def get_track():
+def get_playlist():
+    userid = g.user.get_id()
+
+    list_songs = []
+
+    songs = sinder.playlist.find({'username': userid})
+
+    for song in songs:
+        if not song.has_key('artwork_url'):
+            song['artwork_url'] = 'http://i.imgur.com/bLrm4qD.jpg?1'
+        list_songs.append(song)
+
+    print "reached ehre"
+    return render_template('playlist.html', list_songs=list_songs)
+
+
+def get_track(username):
     """
     get track info from azure recommendation engine for particular user
     """
-    userid = g.user.get_id()
+    return jsonify(get_track_details(username)), 200
 
-    # c = sinder.queue.find({'userid': userid, 'status': 'yes'})
-    # if c.count() > 0:
-    #     c[0].update()
+def get_track_details(username=""):
+    """
+    :param track_no: track number as on sound cloud
+    :return: fetch and return image, stream url, artist info from sound cloud
+    """
+
+    try:
+        userid = g.user.username
+    except:
+        userid = username
 
     print userid
-    tracks = get_from_azure(str(userid))[0]
+    username = userid
 
-    # for track in tracks:
-    # return jsonify(get_track_details(tracks)), 200
 
-    pprint(tracks)
-    return get_track_details(tracks)
+    get_songs = sinder.songsdb.find({'username': username, 'heard': 0})
+    if get_songs.count() > 0:
+        track = get_songs[0]
+        track_no = track['track_no']
+        track['heard'] = 1
+        sinder.songsdb.update({'_id': track['_id']}, track)
+    else:
+        track_nos = get_from_azure(sumchar(userid))
 
+        print len(track_nos)
+
+
+        for track_no in track_nos:
+            print "inserting" + track_no  + " + now"
+            if sinder.songsdb.find({'track_no': track_no, 'username': userid}).count() == 0:
+                sinder.songsdb.insert_one({'track_no': track_no, 'username': userid, 'heard': 0})
+
+        track_no = track_nos[0]
+        sinder.songsdb.update({'track_no': track_no, 'username': userid}, {'track_no': track_no, 'username': userid, 'heard': 1})
+
+    # fetch track to stream
+    track = client.get('/tracks/' + str(track_no))
+
+    # get the tracks streaming URL
+    stream_url = client.get(track.stream_url, allow_redirects=False)
+    artwork_url = str(track.artwork_url).replace("large", "t300x300")
+    title = track.title
+    user = track.user
+
+    if artwork_url == 'None':
+        artwork_url = 'http://i.imgur.com/bLrm4qD.jpg?1'
+
+    ret = {
+        'track_no': str(track_no),
+        'artwork_url': artwork_url,
+        'artist': user['username'],
+        'title': title,
+    }
+
+    copy = {
+        'track_no': str(track_no),
+        'artwork_url': artwork_url,
+        'artist': user['username'],
+        'title': title,
+    }
+
+    sinder.tracklist.insert_one(copy)
+
+    ret['stream_url'] = stream_url.location
+
+    pprint(ret)
+    return ret
 
 @app.route('/register', methods=['POST'])
 def new_user():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if sinder.userdb.find({'username': username }).count() > 0:
+    username = request.form.get('signup_username')
+    password = request.form.get('signup_password')
+    pprint(request.form)
+    pprint (username)
+    pprint(password)
+    if sinder.userdb.find({'username': username}).count() > 0:
         # pain bro, username already exists
-        return jsonify({'error': 'User already exists', 'username': username}), 200
+        return jsonify({'error': 'User already exists', 'username': username, 'status': 0}), 200
 
     user = User(username=username, password=password)
-    sinder.userdb.insert_one(user.__dict__)
-    flash("User Registered!")
-    user.is_auth = True
+    print user.__dict__
     login_user(user)
-    return jsonify({'username': user.username, 'done': True}), 200
+
+    sinder.userdb.insert_one(user.__dict__)
+
+    return jsonify({'status': 1}), 200
 
 
-@app.route('/logout')
+@app.route('/logout/')
 @login_required
 def logout():
     logout_user()
+    return redirect('/')
 
 
 @app.route('/login', methods=['POST'])
@@ -145,48 +244,25 @@ def user_login():
 
     if check_user.count() <= 0:
         return redirect('/')
-        return json.dumps(dict(success=0, error_msg="User doesn't exists"))
     else:
         check_user = check_user[0]
         if check_user['username'] == username and check_user['password'] == password:
             # login successful
-            user = User(_id= check_user['_id'], username=username, password=password)
+            user = User(username=username, password=password)
             login_user(user)
-            flash("Login Successful!")
+
             return redirect('/swipe')
-            return jsonify(status=1)
         else:
             # TODO Add reader for these message on main html pages !!
-            flash("Invalid Credentials")
-            return redirect('/')
-            return jsonify(status=0, error_msg="Invalid Credentials")
+
+            return jsonify({'error': 'Invalid Credentials', 'status': 0}), 200
 
 
-def get_track_details(track_no):
-    """
-    :param track_no: track number as on sound cloud
-    :return: fetch and return image, stream url, artist info from sound cloud
-    """
-    # fetch track to stream
-    track = client.get('/tracks/' + str(track_no))
+def sumchar(s):
+    ret = 0
+    for ch in s:
+        ret += ord(ch)
 
-    # get the tracks streaming URL
-    stream_url = client.get(track.stream_url, allow_redirects=False)
-    artwork_url = str(track.artwork_url).replace("large", "t500x500")
-    title = track.title
-    user = track.user
-
-    if artwork_url == 'None':
-        artwork_url = 'http://i.imgur.com/bLrm4qD.jpg?1'
-
-    ret = {
-        'track_no': str(track_no),
-        'stream_url': stream_url.location,
-        'artwork_url': artwork_url,
-        'artist': user['username'],
-        'title': title,
-    }
-    pprint(ret)
     return ret
 
 if __name__ == '__main__':
